a98af59b4bdc37090ade7ec2b75a571da35edb70
Cenk Kılıç <cenk@kilic.dev>
2023-11-01 20:42:40 +0100

    fix: ext

--- supervisor/.eslintrc.cjs
+++ supervisor/.eslintrc.cjs
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [ '../.eslintrc.js' ],
+  extends: [ '../.eslintrc.cjs' ],
   rules: {
     ...require('@cenk1cenk2/eslint-config/utils').generateImportGroups({ tsconfigDir: __dirname })
   }
